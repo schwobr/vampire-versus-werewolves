465eb9bba3b14150f88a40a396f5defae4bd2adc
Robin Schwob <robin.schwob@student.ecp.fr>
2018-10-24 13:26:33 +0200

    example

--- ServerConnection.py
+++ ServerConnection.py
@@ -2,18 +2,25 @@
 import socket
 
 class ClientThread():
-    def __init__(self):
+    def __init__(self, buff_size):
         self.serverSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.buff_size=buff_size
 
     def connect(self, ip, port):
         self.serverSocket.connect((ip,port))
         print("Connection to %s on port %s" %(ip,port))
+        data=input("Choose your name : ")
+        self.send("NME "+data.length+" "+data)    
 
-    def receive(self, buff_size):
-        res=self.serverSocket.recv(buff_size)
+    def receive(self):
+        return self.serverSocket.recv(self.buff_size)
 
     def send(self, data):
         self.serverSocket.send(data)
+        parsedData=data.split(" ")
+        if parsedData[0]=="NME":
+            self.receive()
+
     
 
 TCP_IP = "127.0.0.1"
