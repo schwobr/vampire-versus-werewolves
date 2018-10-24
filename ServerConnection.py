f2d2a1d3b001487d2d5f911bc6a4caa145f9a11d
Robin Schwob <robin.schwob@student.ecp.fr>
2018-10-24 14:51:53 +0200

    progressing in client-server communication

---
+++ ServerConnection.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+import socket
+import argparse
+import struct
+
+
+class ClientThread():
+    def __init__(self, buff_size):
+        self.serverSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.buff_size=buff_size
+
+    def connect(self, ip, port):
+        self.serverSocket.connect((ip,port))
+        print("Connection to %s on port %s" %(ip,port))
+        data=input("Choose your name : ")
+        self.send("NME%s%s" %(data.length,data))    
+
+    def receive_data(self, size, fmt):
+    data = bytes()
+    while len(data) < size:
+        data += self.serverSocket.recv(size - len(data))
+    return struct.unpack(fmt, data)
+
+    def receive(self, expected):
+        received = self.serverSocket.recv(3).decode("ascii") 
+        if received==expected:  
+            if received=="SET":
+                data=bytes()
+                n,m=self.receive_data(2,"2B")
+                return [(n,m)]+self.receive("HUM")
+            elif received=="HUM":
+                res=[]
+                n= self.receive_data(1,"1B")[0]
+                homes= self.receive_data(2*n,"{}B".format(2*n))
+                count=0
+                prev=0
+                for h in homes:
+                    if count%2==0:
+                        prev=h
+                    else:
+                        res.append((prev,h))
+                    count++
+                return res+self.receive("HME")
+            elif received=="HME":
+                start_pos=tuple(self.receive_data(2,"2B"))
+                return [start_pos]
+        else:
+            print("Error at "+expected)
+
+    def send(self, data):
+        self.serverSocket.send(data)
+        parsedData=data.split(" ")
+        if parsedData[0]=="NME":
+            self.receive()
+
+
+    
+
+
