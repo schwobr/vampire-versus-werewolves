6841bd26933cad372df9daf88aa8281790a718fc
Robin Schwob <robin.schwob@student.ecp.fr>
2018-10-24 12:02:08 +0200

    beginning creation of client class

--- ServerConnection.py
+++ ServerConnection.py
@@ -1,4 +1,19 @@
+#coding: utf-8
 import socket
+
+class ClientThread():
+    def __init__(self):
+        self.serverSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def connect(ip, port):
+        self.serverSocket.connect((ip,port)
+        print("Connection to "+ip+" on port "+str(port))
+
+    def receive(buff_size):
+        res=self.serverSocket.recv(buff_size)
+
+    def send()
+    
 
 TCP_IP = "127.0.0.1"
 TCP_PORT = 5555
