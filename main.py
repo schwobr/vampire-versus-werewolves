a2319545bd7a68e38a40f1be3d3c66d9931c552a
schwobr <robin.schwob@student.ecp.fr>
2018-10-30 10:49:57 +0100

    initializing variables

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ TCP_IP = "127.0.0.1"
 TCP_PORT = 5555
 
 client = ServerConnection.ClientThread()
-client.connect(TCP_IP,TCP_PORT)
+res = client.connect(TCP_IP,TCP_PORT)
+n,m=res[0]
+n_houses=res[1]
+hum=res[2:2+n_houses]
+x,y=res[2+n_houses]
+Map=res[3+n_houses]
+print(n)
+print(m)
+print(n_houses)
+print(hum)
+print(x)
+print(y)
+print(res[3+n_houses:])
