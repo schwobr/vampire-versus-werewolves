8897cd42c77abe84eed504074da5320832b8c7f2
schwobr <robin.schwob@student.ecp.fr>
2018-10-30 12:24:53 +0100

    CA BUG PLUS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ServerConnection
+import map
 
 TCP_IP = "127.0.0.1"
 TCP_PORT = 5555
@@ -9,11 +10,10 @@ n,m=res[0]
 n_houses=res[1]
 hum=res[2:2+n_houses]
 x,y=res[2+n_houses]
-Map=res[3+n_houses]
-print(n)
-print(m)
-print(n_houses)
-print(hum)
-print(x)
-print(y)
-print(res[3+n_houses:])
+Map=res[3+n_houses:]
+print(Map)
+tray = map.Tray(n,m,x,y,Map)
+print(tray.humans)
+print(tray.vampires)
+print(tray.werewolves)
+
