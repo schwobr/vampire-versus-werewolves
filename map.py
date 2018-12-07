d38903b6674c229ac225de2a0064de28e751bd6e
schwobr <robin.schwob@student.ecp.fr>
2018-12-07 17:31:19 +0100

    clarified map.py code

--- map.py
+++ map.py
@@ -9,130 +9,105 @@ import numpy as np
 class Tray():
     
     def __init__(self, N, M, x, y, Map):        
-        self.N=N
-        self.M=M
-        self.N_humans=0
-        self.N_vampires=0
-        self.N_werewolves=0
-        self.humans=[]
-        self.vampires=[]
-        self.werewolves=[]
-        self.MAP=np.zeros((N,M,2),int)
+        self.N = N
+        self.M = M
+        self.N_humans = 0
+        self.N_vampires = 0
+        self.N_werewolves = 0
+        self.humans = []
+        self.vampires = []
+        self.werewolves = []
+        self.MAP = np.zeros((N, M, 2), int)
         self.UpdateTray(Map)
-        self.Type=self.CheckPlayer(Map,x,y)
+        self.Type = self.CheckPlayer(Map, x, y)
      
         
-    def UpdateTray(self,liste):
+    def UpdateTray(self, liste):
         for element in liste:
-            if element[2]!=0:
-                self.MAP[element[1],element[0],0]=1
-                self.MAP[element[1],element[0],1]=element[2]
-            elif element[3]!=0:
-                self.MAP[element[1],element[0],0]=2
-                self.MAP[element[1],element[0],1]=element[3]
-            elif element[4]!=0:
-                self.MAP[element[1],element[0],0]=3
-                self.MAP[element[1],element[0],1]=element[4]
+            if element[2] != 0:
+                self.MAP[element[1], element[0], 0] = 1
+                self.MAP[element[1], element[0], 1] = element[2]
+            elif element[3] != 0:
+                self.MAP[element[1], element[0], 0] = 2
+                self.MAP[element[1] ,element[0], 1] = element[3]
+            elif element[4] != 0:
+                self.MAP[element[1], element[0], 0] = 3
+                self.MAP[element[1], element[0], 1] = element[4]
             else:
-                self.MAP[element[1],element[0],0]=0
-                self.MAP[element[1],element[0],1]=0
+                self.MAP[element[1], element[0], 0] = 0
+                self.MAP[element[1], element[0], 1] = 0
             self.updateLists()
             self.count()
         
-    def CheckPlayer(self,liste, x, y):
+    def CheckPlayer(self, liste, x, y):
         for el in liste:
-            if el[0]==x and el[1]==y:
-                if el[3]!=0:
+            if el[0] == x and el[1] == y:
+                if el[3] != 0:
                     return 2
-                elif el[4]!=0:
+                elif el[4] != 0:
                     return 3
     
     def count(self):
-        self.N_humans=0
-        self.N_vampires=0
-        self.N_werewolves=0
+        self.N_humans = 0
+        self.N_vampires = 0
+        self.N_werewolves = 0
         for x in range(self.N):
             for y in range(self.M):
-                if self.MAP[x,y,0]!=0:
-                    i=self.MAP[x,y,0]
-                    if i==1:
-                        self.N_humans+=1
-                    elif i==2:
-                        self.N_vampires+=1
-                    elif i==3:
-                        self.N_werewolves+=1
+                if self.MAP[x, y, 0] != 0:
+                    i = self.MAP[x, y, 0]
+                    if i == 1:
+                        self.N_humans += 1
+                    elif i == 2:
+                        self.N_vampires += 1
+                    elif i == 3:
+                        self.N_werewolves += 1
 
     def updateLists(self):
-        self.humans=[]
-        self.vampires=[]
-        self.werewolves=[]
+        self.humans = []
+        self.vampires = []
+        self.werewolves = []
         for x in range(self.N):
             for y in range(self.M):
-                if self.MAP[x,y,0]!=0:
-                    i=self.MAP[x,y,0]
-                    j=self.MAP[x,y,1]
-                    if i==1:
-                        self.humans.append((y,x,j))
-                    elif i==2:
-                        self.vampires.append((y,x,j))
-                    elif i==3:
-                        self.werewolves.append((y,x,j))
+                if self.MAP[x, y, 0] != 0:
+                    i = self.MAP[x, y, 0]
+                    j = self.MAP[x, y, 1]
+                    if i == 1:
+                        self.humans.append((y, x, j))
+                    elif i == 2:
+                        self.vampires.append((y, x, j))
+                    elif i == 3:
+                        self.werewolves.append((y, x, j))
 
     def stupidAI(self):
         moves = []
+        test = self.Type == 2
+        us = self.vampires if test else self.werewolves
+        them = self.werewolves if test else self.vampires
         n=0
-        print(self.N_humans)
-        if self.Type==2:
-            for v in self.vampires:
-                x = 0
-                y = 0
-                mini = 100000
-                if self.N_humans>0:
-                    for h in self.humans:
-                        d = (h[0] - v[0])**2 + (h[1] - v[1])**2
-                        if d < mini :
-                            mini = d
-                            x=h[0]
-                            y=h[1]
-                else :
-                    print(self.werewolves)
-                    for w in self.werewolves:
-                        d = (w[0] - v[0])**2 + (w[1] - v[1])**2
-                        if d < mini :
-                            mini = d
-                            x=w[0]
-                            y=w[1]
-                moves.append(v[0])
-                moves.append(v[1])
-                moves.append(v[2])
-                moves.append(v[0]+np.sign(x-v[0]))
-                moves.append(v[1]+np.sign(y-v[1]))
-                n+=1
-        else:
-            for w in self.werewolves:
-                x = 0
-                y = 0
-                mini = 100000
-                if self.N_humans>0:
-                    for h in self.humans:
-                        d = (h[0] - w[0])**2 + (h[1] - w[1])**2
-                        if d < mini :
-                            mini = d
-                            x=h[0]
-                            y=h[1]
-                else :
-                    for v in self.vampires:
-                        d = (w[0] - v[0])**2 + (w[1] - v[1])**2
-                        if d < mini :
-                            mini = d
-                            x=v[0]
-                            y=v[1]
-                moves.append(w[0])
-                moves.append(w[1])
-                moves.append(w[2])
-                moves.append(w[0]+np.sign(x-w[0]))
-                moves.append(w[1]+np.sign(y-w[1]))
-                n+=1
+        for u in us:
+            x = 0
+            y = 0
+            mini = 100000
+            if self.N_humans>0:
+                for h in self.humans:
+                    d = (h[0] - u[0])**2 + (h[1] - u[1])**2
+                    if d < mini :
+                        mini = d
+                        x = h[0]
+                        y = h[1]
+            else :
+                for t in them:
+                    d = (t[0] - u[0])**2 + (t[1] - u[1])**2
+                    if d < mini :
+                        mini = d
+                        x = t[0]
+                        y = t[1]
+            moves.append(u[0])
+            moves.append(u[1])
+            moves.append(u[2])
+            moves.append(u[0] + np.sign(x - u[0]))
+            moves.append(u[1] + np.sign(y - u[1]))
+            n += 1
         print(moves)
         return ["MOV", n, moves]
 
